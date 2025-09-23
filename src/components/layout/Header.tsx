588f833a394aae0a75c51ba7a440719ef4a4512c
Diego <darc16066@gmail.com>
2025-09-23 00:09:21 -0500

    fix: move images to public/assets and update paths in Home.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,7 +9,7 @@ const Header: React.FC = () => {
           <div className="flex items-center space-x-6">
             <div className="w-20 h-20 flex items-center justify-center">
               <img 
-                src="public\assets\Tibuchina.png" 
+                src="/assets/Tibuchina.png" 
                 alt="Tibuchina Logo" 
                 className="w-20 h-20 object-contain"
               />
